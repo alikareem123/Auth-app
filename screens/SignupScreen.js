7df80364c9889952f23dee5dd090eb911c9d298f
alikareem123 <55332168+alikareem123@users.noreply.github.com>
2024-07-19 09:11:28 +0530

    Authentication code complete

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,29 @@
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  async function signUpHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Authentication failed", "Failed to log you in. Try later!");
+      setIsAuthenticating(false);
+    }
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating a User..." />;
+  }
+  return <AuthContent onAuthenticate={signUpHandler} />;
 }
 
 export default SignupScreen;
